ae435bc9b23204e4e8e608fccf76c6be631a7b3e
Jitesh Khurana <jkhurana_be21@thapar.edu>
2023-11-22 15:30:33 +0530

    email,name and password validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import Header from './Header'
-import {useState} from 'react'
+import {useState,useRef} from 'react'
+import { checkValidData } from '../utils/validate'
 
 const Login = () => {
   const [isSignIn,setIsSignIn] = useState(true);
+  const [errMessage,setErrMessage] = useState(null);
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
   const toggleSignInForm=()=>{
     setIsSignIn(!isSignIn);
+  }
+  const handleButtonClick=()=>{
+    // Validate The form data
+    const message=checkValidData(name.current.value,email.current.value,password.current.value);
+    setErrMessage(message);
   }
   return (
     <div>
@@ -13,12 +23,13 @@ const Login = () => {
       <div className='absolute'>
         <img src='https://assets.nflxext.com/ffe/siteui/vlv3/a09bb938-2d90-42ae-986e-5a3e4abf9e77/8eb1e781-3494-4aa4-9405-268ca6473e4c/IN-en-20231113-popsignuptwoweeks-perspective_alpha_website_large.jpg' alt='bg-img'/>
       </div>
-      <form className=' w-3/12 h-3/5 absolute bg-black my-36 p-10 mx-auto left-0 right-0 bg-opacity-90 rounded-lg text-white '>
+      <form onSubmit={(e)=>e.preventDefault()} className=' w-3/12 h-3/5 absolute bg-black my-36 p-10 mx-auto left-0 right-0 bg-opacity-82 rounded-lg text-white '>
         <h1 className='text-3xl py-4 m-2 font-bold'>{isSignIn?"Sign In":"Sign Up"}</h1>
-        {!isSignIn && <input type='text' placeholder='Full Name' className='p-3 my-2 w-full rounded-md bg-gray-700'></input>}
-        <input type='text' placeholder='Email Address' className='p-3 my-2 w-full rounded-md bg-gray-700'></input>
-        <input type='password' placeholder='Password' className='p-3 my-2 w-full rounded-md bg-gray-700'></input>
-        <button className='p-3 mt-6 mb-2 w-full text-white bg-red-600 rounded-md'>{isSignIn?"Sign In":"Sign Up"}</button>
+        {!isSignIn && <input ref={name} type='text' placeholder='Full Name' className='p-3 my-2 w-full rounded-md bg-gray-700'></input>}
+        <input ref={email} type='email' autoComplete="username" placeholder='Email Address' className='p-3 my-2 w-full rounded-md bg-gray-700'></input>
+        <input ref={password} type='password' autoComplete="current-password" placeholder='Password' className='p-3 my-2 w-full rounded-md bg-gray-700'></input>
+        <p className='mt-2 text-yellow-300'>{errMessage}</p>
+        <button className='p-3 mt-6 mb-2 w-full text-white bg-red-600 rounded-md' onClick={handleButtonClick}>{isSignIn?"Sign In":"Sign Up"}</button>
         <p className='mt-2 cursor-pointer hover:underline' onClick={toggleSignInForm}>{isSignIn?"New to Netfix? Sign up now.":"Already a User? Sign in"}</p>
       </form>
     </div>
---
+++ src/utils/validate.js
@@ -0,0 +1,9 @@
+export const checkValidData=(name,email,password)=>{
+    const isNameValid = /^[a-zA-Z]+$/.test(name);
+    const isEmailValid = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email);
+    const isPasswordValid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/.test(password)
+    if(!isNameValid) return "Invalid Name";
+    if(!isEmailValid) return "Invalid Email ID";
+    if(!isPasswordValid) return "Invalid Password";
+    return null;
+}
